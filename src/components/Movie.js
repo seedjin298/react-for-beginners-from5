0b19fb02d00e91e0615735c0e35b1569ef7a9295
seedjin298 <seedmemoria@gmail.com>
2023-01-08 22:41:17 +0900

    #7.5 React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+//Link는 하이퍼링크 눌렀을 때 사이트가 reflesh되는거 방지 -> 로딩 빠름
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={"/movie"}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
