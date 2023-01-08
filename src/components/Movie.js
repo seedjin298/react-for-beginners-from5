e1f0f206009853f90607f465cba357d0996b8372
seedjin298 <seedmemoria@gmail.com>
2023-01-08 22:51:58 +0900

    #7.6 Parameters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 //Link는 하이퍼링크 눌렀을 때 사이트가 reflesh되는거 방지 -> 로딩 빠름
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to={"/movie"}>{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -20,6 +20,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
